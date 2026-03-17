4d73c063942ce56ee796b1d79461e99c9416fef4
Tchips46 <leo.out46@gmail.com>
2026-03-17 12:16:29 +0900

    fix(kill): put kill in bootstrap

--- src/bin/nf.ts
+++ src/bin/nf.ts
@@ -8,18 +8,18 @@ import { loadLocalBinCommandLoader, localBinExists } from "@utils/local-binaries
 
 import { CommandLoader } from "~/command";
 
-const signals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGHUP", "SIGQUIT", "SIGBREAK"];
-
-signals.forEach((signal) => {
-  const listener = async () => {
-    process.off(signal, listener);
-    await treeKill(process.pid, signal);
-  };
-
-  process.on(signal, listener);
-});
-
 const bootstrap = async () => {
+  const signals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGHUP", "SIGQUIT", "SIGBREAK"];
+
+  signals.forEach((signal) => {
+    const listener = async () => {
+      process.off(signal, listener);
+      await treeKill(process.pid, signal);
+    };
+
+    process.on(signal, listener);
+  });
+
   program
     .version(
       (await import("../../package.json")).version ?? "unknown",
